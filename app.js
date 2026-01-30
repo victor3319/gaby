127333b618d4f0d3ae51bd640e14de36d1d3a260
victor <victor3319.vc@gmail.com>
2026-01-30 07:57:31 -0300

    30012026

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const fs = require("fs");
 const path = require ("path");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const pdf = require("pdf-parse-new")
 
 
 const statics = path.join(__dirname,"archivos")
@@ -33,6 +35,10 @@ app.use((req, res, next) =>{
 //express bases
 app.set("bases", path.join(__dirname,"./archivos/bases"))
 
+//leer pdf
+const dataBuffer = fs.readFileSync('./uploads/recibos/archivo-.pdf');
+pdf(dataBuffer).then(function(datos){console.log(datos.numpages)})
+// Número de páginas console.log ( datos.text ) ; // Contenido de texto completo console.log ( datos.info ) ; // Metadatos del PDF } ) ;)})
 
 
 module.exports = app
--- archivos/funciones_rutas/index.js
+++ archivos/funciones_rutas/index.js
@@ -7,6 +7,7 @@ const path = require ("path");
 const multer = require("multer");
 
 
+
 var usuario = []
 
 //Configurar almacenamiento
