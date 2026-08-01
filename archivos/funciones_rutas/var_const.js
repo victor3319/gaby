abbe388543264d42622860103ab138b68b431860
victor <victor3319.vc@gmail.com>
2026-07-31 23:09:13 -0300

    31072026-4

--- archivos/funciones_rutas/var_const.js
+++ archivos/funciones_rutas/var_const.js
@@ -3,32 +3,11 @@ const path = require("path");
 
     //CONSTANTES
 
-    //Base de Usuarios
-    const bUsuarios = fs.readFileSync("./archivos/bases/usuarios.json", "utf-8");
-    const usuarios = JSON.parse(bUsuarios);
-    
-    /*//Base de recibos
-    const bRecibos = fs.readFileSync("archivos/bases/recibos.json", "utf-8");
-    const recibos = JSON.parse(bRecibos)*/
-    
-    //Datos de Usuario en uso
-    //const bUsuarioEnUso = fs.readFileSync("./archivos/bases/UsuarioEnUso.json", "utf-8");
-    const bUsuarioEnUso = fs.readFileSync(path.join("./archivos/bases/usuarioEnUso.json"));
-    const usuarioEnUso = JSON.parse(bUsuarioEnUso);
-
     //Procesos
     const baseProceso = fs.readFileSync("./archivos/bases/procesos.json", "utf-8");
     const procesos = JSON.parse(baseProceso)
 
-    //Solicitudes
-    const rutaSolicitudes = "./archivos/bases/solicitudesEmpleos.json"
-    const objetoSolicitudes =JSON.parse(fs.readFileSync("./archivos/bases/solicitudesEmpleos.json"))
-
+   
 module.exports = {
-    usuarios : usuarios,
-    //recibos : recibos,
-    usuarioEnUso : usuarioEnUso,
-    procesos : procesos,
-    rutaSolicitudes : rutaSolicitudes,
-    objetoSolicitudes : objetoSolicitudes
+    procesos : procesos
 }
